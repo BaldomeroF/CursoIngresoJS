b02aefec35d9f9a96f3bdecff30cdc5b75a1cd62
BaldomeroF <36193612+BaldomeroF@users.noreply.github.com>
2018-02-06 10:15:59 -0300

    Ejercicios 1 y 2
    Alert, concadenar, prompt y var

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,17 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
-}
+	/*
+	var nombre;
+	alert(nombre);
+	alert("nombre");
+	nombre = "Baldomero Fiorentino";
+	alert(nombre);
+    var person = prompt("Please enter your name", "Harry Potter");
+    */
 
+    var persona;
+    persona = prompt("Escribi tu nombre", "");
+    //1er texto entre comillas, será el texto mostrado en pantalla
+    alert("Tu nombre es: "+persona);
+}
