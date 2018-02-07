f122167796b9380f69f2f37028f37c083457573d
BaldomeroF <36193612+BaldomeroF@users.noreply.github.com>
2018-02-07 10:27:27 -0300

    2
    porcentaje

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,7 +1,14 @@
 function Mostrar()
 {
-  var producto;
-  producto = prompt ("Ingrese valor del producto");
-  var productofinal;
-  productofinal = producto+
+  var importe;
+
+  importe =prompt(importe);
+  importe = importe*0.21;
+
+  /*para restar un porcentaje a un importe agregarle el porcentaje a 0
+  para sumar porcentaje a un importe restarle el porcentaje a 0
+  mediante la multiplicacion */
+  
+  document .getElementById('importeFinal').value=importe;
+
 }
