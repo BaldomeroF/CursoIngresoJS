98a83073e4fcdf0daa7480427046bae64bd4f4fb
BaldomeroF <36193612+BaldomeroF@users.noreply.github.com>
2018-02-07 12:18:54 -0300

    1, 2, 3, 4, 5 y 6
    If

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,22 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
 
+	edad= document .getElementById('edad').value;
 
+	if(edad<=12)
+	{
+		alert("Sos un nene");
+	}
+	else
+	{
 
+		if(edad>17)
+			alert("Sos un adulto");
+
+		else
+			alert("Sos un adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
